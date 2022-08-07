01fba0b1e9b85c005c9828877bb0d0a66a5c903e
Julian Espitia <julicam156@gmail.com>
2022-08-07 18:53:34 -0500

    Se creo la interfaz para la tarjeta de credito

--- lib/models/utilities.dart
+++ lib/models/utilities.dart
@@ -2,3 +2,4 @@ export 'package:google_fonts/google_fonts.dart';
 export 'package:flutter/gestures.dart';
 export 'package:flutter/material.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
--- lib/screens/info_tarjeta_screens.dart
+++ lib/screens/info_tarjeta_screens.dart
@@ -9,6 +9,40 @@ class InfoTarjetaPage extends StatefulWidget {
 }
 
 class _InfoTarjetaPageState extends State<InfoTarjetaPage> {
+  var cardMask = MaskTextInputFormatter(
+      mask: '#### #### #### ####', filter: {"#": RegExp(r'[0-9]')});
+
+  var dateMask =
+      MaskTextInputFormatter(mask: '##/##', filter: {"#": RegExp(r'[0-9]')});
+
+  var codeMask =
+      MaskTextInputFormatter(mask: '###', filter: {"#": RegExp(r'[0-9]')});
+  String nombre = '';
+  String card = '';
+  String fecha = '';
+  String codigo = '';
+  final formKey = GlobalKey<FormState>();
+  final nombreController = TextEditingController();
+  final cardController = TextEditingController();
+  final fechaController = TextEditingController();
+  final codigoController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    nombreController.dispose();
+    cardController.dispose();
+    fechaController.dispose();
+    codigoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -30,26 +64,199 @@ class _InfoTarjetaPageState extends State<InfoTarjetaPage> {
                     color: Colors.black,
                     size: 20,
                   ),
+                  _creditCard(context),
                   Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: SizedBox(
-                      height: 100.0,
-                      width: 300,
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          helperMaxLines: 16,
-                          hintText: 'Número de tarjeta',
-                          label: Text('Número de tarjeta'),
-                          border: OutlineInputBorder(),
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: _inputNombre(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: _inputCard(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0, left: 25.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          child: _inputDate(),
                         ),
-                      ),
+                        Container(
+                          child: _inputCode(),
+                        )
+                      ],
                     ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 160.0),
+                    child: BotonWidget(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      textColor: Theme.of(context).colorScheme.primary,
+                      text: 'Continuar',
+                      navigator: '',
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _creditCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Container(
+        height: 180,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, bottom: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TitleText(
+                text: card == '' ? 'Número de tarjeta' : card,
+                color: Theme.of(context).colorScheme.secondary,
+                size: 20.0,
+              ),
+              TitleText(
+                text: nombre == '' ? 'Nombre del titular' : nombre,
+                color: Theme.of(context).colorScheme.secondary,
+                size: 20.0,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15.0),
+                    child: TitleText(
+                      text: fecha == '' ? 'Fecha' : fecha,
+                      color: Theme.of(context).colorScheme.secondary,
+                      size: 20.0,
+                    ),
+                  ),
+                  TitleText(
+                    text: codigo == '' ? 'Código' : codigo,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 20.0,
                   ),
                 ],
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Container _inputNombre() {
+    return Container(
+      height: 50,
+      width: 350,
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      margin: const EdgeInsets.symmetric(horizontal: 15.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(50.0)),
+      child: TextFormField(
+        controller: nombreController,
+        keyboardType: TextInputType.name,
+        textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(fontSize: 20.0),
+        onChanged: (nombreInput) {
+          setState(() {
+            nombre = nombreInput;
+          });
+        },
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Su nombre',
+        ),
+      ),
+    );
+  }
+
+  Container _inputCard() {
+    return Container(
+      height: 50,
+      width: 350,
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      margin: const EdgeInsets.symmetric(horizontal: 15.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(50.0)),
+      child: TextFormField(
+        controller: cardController,
+        inputFormatters: [cardMask],
+        keyboardType: TextInputType.number,
+        style: const TextStyle(fontSize: 20.0),
+        onChanged: (cardInput) {
+          setState(() {
+            card = cardInput;
+          });
+        },
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: '0000-0000-0000-0000',
+        ),
+      ),
+    );
+  }
+
+  Container _inputDate() {
+    return Container(
+      height: 50,
+      width: 150,
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      margin: const EdgeInsets.symmetric(horizontal: 15.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(50.0)),
+      child: TextFormField(
+        controller: fechaController,
+        keyboardType: TextInputType.number,
+        inputFormatters: [dateMask],
+        style: const TextStyle(fontSize: 20.0),
+        onChanged: (dateInput) {
+          setState(() {
+            fecha = dateInput;
+          });
+        },
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: 'MM/YYYY',
+        ),
+      ),
+    );
+  }
+
+  Container _inputCode() {
+    return Container(
+      height: 50,
+      width: 150,
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      margin: const EdgeInsets.symmetric(horizontal: 15.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(50.0)),
+      child: TextFormField(
+        controller: codigoController,
+        inputFormatters: [codeMask],
+        keyboardType: TextInputType.number,
+        style: const TextStyle(fontSize: 20.0),
+        onChanged: (codeInput) {
+          setState(() {
+            codigo = codeInput;
+          });
+        },
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: '***',
         ),
       ),
     );
